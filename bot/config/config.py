475887bddba0dea9dc49bd823603564d60ab49d2
sirbiprod <35518075+sirbiprod@users.noreply.github.com>
2024-06-23 23:00:56 +0500

    Edit default settings

--- bot/config/config.py
+++ bot/config/config.py
@@ -7,24 +7,22 @@ class Settings(BaseSettings):
     API_ID: int
     API_HASH: str
 
-    MIN_AVAILABLE_ENERGY: int = 100
-    SLEEP_BY_MIN_ENERGY: int = 200
+    MIN_AVAILABLE_ENERGY: int = 300
+    SLEEP_BY_MIN_ENERGY: int = 314
 
-    ADD_TAPS_ON_TURBO: int = 2500
+    ADD_TAPS_ON_TURBO: int = 2000
 
-    AUTO_BUY_TAPBOT: bool = True
-
-    AUTO_UPGRADE_TAP: bool = True
+    AUTO_UPGRADE_TAP: bool = False
     MAX_TAP_LEVEL: int = 5
-    AUTO_UPGRADE_ENERGY: bool = True
+    AUTO_UPGRADE_ENERGY: bool = False
     MAX_ENERGY_LEVEL: int = 5
-    AUTO_UPGRADE_CHARGE: bool = True
-    MAX_CHARGE_LEVEL: int = 3
+    AUTO_UPGRADE_CHARGE: bool = False
+    MAX_CHARGE_LEVEL: int = 5
 
     APPLY_DAILY_ENERGY: bool = True
     APPLY_DAILY_TURBO: bool = True
 
-    RANDOM_TAPS_COUNT: list[int] = [50, 200]
+    RANDOM_TAPS_COUNT: list[int] = [5, 23]
     SLEEP_BETWEEN_TAP: list[int] = [15, 25]
 
     USE_PROXY_FROM_FILE: bool = False
