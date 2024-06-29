dd075c671fa1a17d02dca58a1c1ff817be3df00a
sirbiprod <35518075+sirbiprod@users.noreply.github.com>
2024-06-30 04:26:55 +0500

    Update graphql.py

--- bot/utils/graphql.py
+++ bot/utils/graphql.py
@@ -13,9 +13,12 @@ class Query(str, Enum):
     TapbotStart = "fragment FragmentTapBotConfig on TelegramGameTapbotOutput {\n  damagePerSec\n  endsAt\n  id\n  isPurchased\n  startsAt\n  totalAttempts\n  usedAttempts\n  __typename\n}\n\nmutation TapbotStart {\n  telegramGameTapbotStart {\n    ...FragmentTapBotConfig\n    __typename\n  }\n}"
     TapbotClaim = "fragment FragmentTapBotConfig on TelegramGameTapbotOutput {\n  damagePerSec\n  endsAt\n  id\n  isPurchased\n  startsAt\n  totalAttempts\n  usedAttempts\n  __typename\n}\n\nmutation TapbotClaim {\n  telegramGameTapbotClaimCoins {\n    ...FragmentTapBotConfig\n    __typename\n  }\n}"
     Mutation = "mutation Mutation {\n  telegramUserClaimReferralBonus\n}"
-    ClanMy = "fragment FragmentClanProfile on ClanProfileOutput {\n  id\n  clanDetails {\n    id\n    name\n    rarity\n    username\n    avatarImageUrl\n    coinsAmount\n    createdAt\n    description\n    membersCount\n    __typename\n  }\n  clanOwner {\n    id\n    userId\n    username\n    avatarImageUrl\n    coinsAmount\n    currentBossLevel\n    firstName\n    lastName\n    isClanOwner\n    isMe\n    __typename\n  }\n  __typename\n}\n\nquery ClanMy {\n  clanMy {\n    ...FragmentClanProfile\n    __typename\n  }\n}"
+    ClanMy = "fragment FragmentClanProfile on ClanProfileOutput {\n id\n clanDetails {\n id\n name\n rarity\n username\n avatarImageUrl\n coinsAmount\n createdAt\n description\n membersCount\n __typename\n }\n clanOwner {\n id\n userId\n username\n avatarImageUrl\n coinsAmount\n currentBossLevel\n firstName\n lastName\n isClanOwner\n isMe\n __typename\n }\n __typename\n}\n\nquery ClanMy {\n clanMy {\n ...FragmentClanProfile\n __typename\n }\n}"
     Leave = "mutation Mutation {\n  clanActionLeaveClan\n}"
     Join = "mutation ClanActionJoinClan($clanId: String!) {\n clanActionJoinClan(clanId: $clanId)\n}"
+    #clanProfile = "fragment FragmentClanProfile on ClanProfileOutput {\n id\n clanDetails {\n id\n name\n rarity\n username\n avatarImageUrl\n coinsAmount\n createdAt\n description\n membersCount\n __typename\n }\n clanOwner {\n id\n userId\n username\n avatarImageUrl\n coinsAmount\n currentBossLevel\n firstName\n lastName\n isClanOwner\n isMe\n __typename\n }\n __typename\n}\n\nquery clanProfile($clanId: String!) {\n clanProfile(clanId: $clanId) {\n ...FragmentClanProfile\n __typename\n }\n}"
+    #ClanMembersPaginated = "query ClanMembersPaginated($clanId: String!, $pagination: PaginationInput!) {\n clanMembersPaginated(clanId: $clanId, pagination: $pagination) {\n items {\n id\n userId\n username\n firstName\n lastName\n avatarImageUrl\n coinsAmount\n currentBossLevel\n isClanOwner\n isMe\n __typename\n }\n meta {\n currentPage\n itemCount\n itemsPerPage\n totalItems\n totalPages\n __typename\n }\n __typename\n }\n}"
+
 class OperationName(str, Enum):
     QUERY_GAME_CONFIG = "QUERY_GAME_CONFIG"
     MutationTelegramUserLogin = "MutationTelegramUserLogin"
@@ -31,3 +34,5 @@ class OperationName(str, Enum):
     ClanMy = "ClanMy"
     Leave = "Mutation"
     Join = "ClanActionJoinClan"
+    #clanProfile = "clanProfile"
+    #ClanMembersPaginated = "ClanMembersPaginated"
