49e53d3c1ad9b2115617b8c058011d53520bfb68
sirbiprod <35518075+sirbiprod@users.noreply.github.com>
2024-06-30 05:41:38 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from bot.utils.launcher import process
 async def main():
     if sys.version_info >= (3,12):
         exit("Python 3.11 or lower is required. Please downgrade your Python version else this code will not work properly.")
+        
     await process()
 
 
